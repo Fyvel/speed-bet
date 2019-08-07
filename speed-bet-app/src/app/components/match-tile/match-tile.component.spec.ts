2237536237c6b12314f7f12fe93ecb1930287d9b
Fyvel <fdelage8000@gmail.com>
2019-08-07 18:11:01 +0200

    unit testing for match-tile comp

--- speed-bet-app/src/app/components/match-tile/match-tile.component.spec.ts
+++ speed-bet-app/src/app/components/match-tile/match-tile.component.spec.ts
@@ -1,25 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MatchTileComponent } from './match-tile.component';
+import { MatchModel } from 'src/app/models/interfaces';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('MatchTileComponent', () => {
   let component: MatchTileComponent;
   let fixture: ComponentFixture<MatchTileComponent>;
+  const dummyInput = {
+    matchId: 123
+  } as MatchModel;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MatchTileComponent ]
+      declarations: [MatchTileComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MatchTileComponent);
     component = fixture.componentInstance;
+    component.match = dummyInput;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should assign the match route API segment', () => {
+    // arrange
+    const expectedRoute = `/match/${dummyInput.matchId}`;
+    // act
+    component.ngOnInit();
+    // assert
+    expect(component.matchRoute).toEqual(expectedRoute);
   });
 });
