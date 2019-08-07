97f81c4e19990a7745a477d06bde220870b27106
Fyvel <fdelage8000@gmail.com>
2019-08-07 18:00:57 +0200

    unit test for matches component

--- speed-bet-app/src/app/components/matches/matches.component.spec.ts
+++ speed-bet-app/src/app/components/matches/matches.component.spec.ts
@@ -1,25 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MatchesComponent } from './matches.component';
+import { MatchesService } from 'src/app/services/matches.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-describe('MatchesComponent', () => {
+fdescribe('MatchesComponent', () => {
   let component: MatchesComponent;
   let fixture: ComponentFixture<MatchesComponent>;
+  let service: MatchesService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MatchesComponent ]
+      imports: [HttpClientModule],
+      declarations: [MatchesComponent],
+      providers: [MatchesService],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MatchesComponent);
     component = fixture.componentInstance;
+    service = TestBed.get(MatchesService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get latest matches', () => {
+    // arrange
+    const spy = spyOn(service, 'getLatestMatches');
+    // act
+    component.ngOnInit();
+    // assert
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
